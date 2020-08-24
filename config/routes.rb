5735b4ec77148db08e99d57336032785988e25b0
Pauline Coupez <coupez.pauline@gmail.com>
2020-08-24 18:23:49 +0200

    add boostrap and simpleform

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
-  get 'products', to: 'pages#products'
+  resources :products
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
