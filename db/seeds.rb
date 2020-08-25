6cfc675890b11c15e7fbcebcf9c8c2ed146c34fa
Pauline Coupez <coupez.pauline@gmail.com>
2020-08-25 17:05:23 +0200

    WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ puts 'Create products...'
 product = Product.create(title: "Airpods Pro", description: "An awesome product")
 Variant.create(sku: 'BG-101', stock_quantity: 42, product_id: product.id)
 product = Product.create(title: "Beats by Dre", description: "A greaaaat product")
-Variant.create(sku: 'BG-101', stock_quantity: 13, product_id: product.id)
+Variant.create(sku: 'BG-102', stock_quantity: 13, product_id: product.id)
 product = Product.create(title: "Bose 700", description: "A perfect product")
-Variant.create(sku: 'BG-101', stock_quantity: 0, product_id: product.id)
+Variant.create(sku: 'BG-103', stock_quantity: 0, product_id: product.id)
 puts "Youpi!"
