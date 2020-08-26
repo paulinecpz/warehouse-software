b255cd60453963d269a128475627240c1ef49a22
Pauline Coupez <coupez.pauline@gmail.com>
2020-08-26 11:47:46 +0200

    try to set up nested models with simple form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :variants, dependent: :destroy
+  accepts_nested_attributes_for :variants
   validates :description, length: { maximum: 300 }
   default_scope { where.not(soft_deleted: true) }
 end
