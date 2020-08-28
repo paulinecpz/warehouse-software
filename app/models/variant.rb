adc0caae0d8478aa8251c469c8dd7526f5d9db02
Pauline Coupez <coupez.pauline@gmail.com>
2020-08-28 14:46:05 +0200

    stock required and not negative

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -1,5 +1,5 @@
 class Variant < ApplicationRecord
   belongs_to :product
   validates :sku, uniqueness: true, presence: true
-  validates :stock_quantity, presence: true
+  validates :stock_quantity, presence: true, :numericality => { :greater_than => 0 }
 end
