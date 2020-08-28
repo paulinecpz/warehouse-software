877e76eb9be360214f80c0ecdf7ca73266830ef0
Pauline Coupez <coupez.pauline@gmail.com>
2020-08-28 14:39:10 +0200

    add more tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe Product, type: :model do
   it 'raise an error' do 
     expect { Product.create!(description: 'Nice but poor charger') }.to raise_error(ActiveRecord::RecordInvalid)  # test code
   end
+
+  describe '#sync_product_description' do
+    product = Product.create(title: 'Bose 5000', description: 'Wrong description')
+
+    it "update property description" do
+      expect { product.update(description: "New description from Pauline") }.to change { product.reload.description }
+    end
+  end
 end
